66076f629e5999f3d5201b337bc2eb16fec81e73
Rapuška <mikalajsiatkouski@gmail.com>
2023-05-19 02:31:47 +0300

    +main window layout

---
+++ src/OrganizerCore/OrganizerCore/Windows/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Windows;
+using Organizer.DbWorking;
+
+namespace OrganizerCore
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			DataBaseConnection.Instance.OpenDataBase();
+		}
+
+		private void ScheduleButton_Click(object sender, RoutedEventArgs e)
+		{
+			// MainFrame.Content = new Schedule();
+		}
+
+		private void StudentsButton_Click(object sender, RoutedEventArgs e)
+		{
+			// MainFrame.Content = new Student();
+		}
+
+		private void GroupsButton_Click(object sender, RoutedEventArgs e)
+		{
+			// MainFrame.Content = new Group();
+		}
+
+		private void CoursesButton_Click(object sender, RoutedEventArgs e)
+		{
+			// MainFrame.Content = new Course();
+		}
+	}
+}
