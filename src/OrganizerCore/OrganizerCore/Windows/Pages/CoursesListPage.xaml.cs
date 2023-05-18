1fcb0b6a6bfe5525c8aa6e17605e352ce050d877
Rapuška <mikalajsiatkouski@gmail.com>
2023-05-19 02:40:23 +0300

    +CoursesListPage

---
+++ src/OrganizerCore/OrganizerCore/Windows/Pages/CoursesListPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace OrganizerCore.Windows.Pages;
+
+public partial class CoursesListPage : Page
+{
+	public CoursesListPage()
+	{
+		InitializeComponent();
+	}
+
+	private void AddLessonButton_Click(object sender, RoutedEventArgs e)
+	{
+		// NavigationService.Navigate(new View_Subject());
+	}
+
+	private void AddCourseButton_Click(object sender, RoutedEventArgs e)
+	{
+		// NavigationService.Navigate(new AddCourse());
+	}
+}
