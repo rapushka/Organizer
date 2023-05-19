9d14f6b8fd07181a46f7d20278e92a97405fa047
Rapuška <mikalajsiatkouski@gmail.com>
2023-05-19 18:05:17 +0300

    edit

--- src/OrganizerCore/OrganizerCore/Tools/Extension/DataGridExtensions.cs
+++ src/OrganizerCore/OrganizerCore/Tools/Extension/DataGridExtensions.cs
@@ -7,7 +7,12 @@ namespace OrganizerCore.Tools.Extensions;
 public static class DataGridExtensions
 {
 	public static void AddColumn
-		(this DataGrid @this, string header, string binding, Visibility visibility = Visibility.Visible)
+	(
+		this DataGrid @this,
+		string header,
+		string binding,
+		Visibility visibility = Visibility.Visible
+	)
 	{
 		@this.Columns.Add
 		(
--- src/OrganizerCore/OrganizerCore/Windows/Pages/TypesOfLessonsListPage.xaml.cs
+++ src/OrganizerCore/OrganizerCore/Windows/Pages/TypesOfLessonsListPage.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Linq;
 using System.Windows;
+using System.Windows.Controls;
+using Microsoft.EntityFrameworkCore;
 using Organizer.DbWorking;
 using Organizer.Model;
 using OrganizerCore.Tools.Extensions;
@@ -16,16 +18,46 @@ public partial class TypesOfLessonsListPage
 	{
 		TypesOfLessonsDataGrid.ItemsSource = Context.TypesOfLessons.ToList();
 
-		SetupTypesOfLessons();
+		SetupTypesOfLessonsTable();
 	}
 
 	private void OkBase_OnClick(object sender, RoutedEventArgs e) => NavigationService!.GoBack();
 
-	private void SetupTypesOfLessons()
+	private void SetupTypesOfLessonsTable()
 	{
 		TypesOfLessonsDataGrid.Columns.Clear();
 
 		TypesOfLessonsDataGrid.AddColumn("ID", nameof(TypeOfLesson.Id), Visibility.Collapsed);
 		TypesOfLessonsDataGrid.AddColumn("Название", nameof(TypeOfLesson.Title));
+	}
+
+	private void AddButton_OnClick(object sender, RoutedEventArgs e)
+	{
+		var newType = new TypeOfLesson();
+		TypesOfLessonsDataGrid.Items.Add(newType);
+
+		TypesOfLessonsDataGrid.SelectedItem = newType;
+		TypesOfLessonsDataGrid.ScrollIntoView(newType);
+		TypesOfLessonsDataGrid.BeginEdit();
+	}
+
+	private void EditButton_OnClick(object sender, RoutedEventArgs e) { }
+
+	private void RemoveButton_OnClick(object sender, RoutedEventArgs e) { }
+
+	private void TypesOfLessonsDataGrid_OnCellEditEnding(object? sender, DataGridCellEditEndingEventArgs e)
+	{
+		if (e.EditingElement is not TextBox editedCell
+		    || e.Row.DataContext is not TypeOfLesson editedTypeOfLesson)
+		{
+			return;
+		}
+
+		var editedValue = editedCell.Text;
+		editedTypeOfLesson.Title = editedValue;
+
+		Context.Entry(editedTypeOfLesson).State = EntityState.Modified;
+		Context.SaveChanges();
+		MessageBox.Show(string.Join(", ", Context.TypesOfLessons.Select((tol) => tol.Title)));
 	}
 }
