5d3b8f418b8d00251cb1c0715020611c62fa5fba
Rapuška <mikalajsiatkouski@gmail.com>
2023-05-19 16:51:18 +0300

    TopicEditPage filling

--- src/OrganizerCore/OrganizerCore/Windows/Pages/TopicEditPage.xaml.cs
+++ src/OrganizerCore/OrganizerCore/Windows/Pages/TopicEditPage.xaml.cs
@@ -1,16 +1,27 @@
-﻿using System.Windows.Controls;
+﻿using System.Linq;
+using System.Windows;
+using Organizer.DbWorking;
 using Organizer.Model;
 
 namespace OrganizerCore.Windows.Pages;
 
-public partial class TopicEditPage : Page
+public partial class TopicEditPage
 {
-	private Topic _topic;
+	private readonly Topic _topic;
 
 	public TopicEditPage(Topic topic)
 	{
 		_topic = topic;
 
 		InitializeComponent();
+	}
+
+	private static ApplicationContext Context => DataBaseConnection.Instance.CurrentContext;
+
+	private void Page_OnLoaded(object sender, RoutedEventArgs e)
+	{
+		CourseTitleTextBox.Text = _topic.Course.Title;
+		TitleTextBox.Text = _topic.Title;
+		LessonsDataGrid.ItemsSource = Context.Lessons.Where((l) => l.Topic == _topic).ToList();
 	}
 }
