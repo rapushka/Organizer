1f1d8aca8377e1c82adb0e15824f1e66d7355065
Rapuška <mikalajsiatkouski@gmail.com>
2023-05-19 17:10:27 +0300

    LessonsTable Setup

--- src/OrganizerCore/OrganizerCore/Windows/Pages/TopicEditPage.xaml.cs
+++ src/OrganizerCore/OrganizerCore/Windows/Pages/TopicEditPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Windows;
 using Organizer.DbWorking;
 using Organizer.Model;
+using OrganizerCore.Tools.Extensions;
 
 namespace OrganizerCore.Windows.Pages;
 
@@ -23,5 +24,17 @@ public partial class TopicEditPage
 		CourseTitleTextBox.Text = _topic.Course.Title;
 		TitleTextBox.Text = _topic.Title;
 		LessonsDataGrid.ItemsSource = Context.Lessons.Where((l) => l.Topic == _topic).ToList();
+
+		SetupLessonsTable();
+	}
+
+	private void SetupLessonsTable()
+	{
+		LessonsDataGrid.Columns.Clear();
+
+		LessonsDataGrid.AddColumn("ID", nameof(Lesson.Id), Visibility.Collapsed);
+		LessonsDataGrid.AddColumn("Номер занятия", nameof(Lesson.Number));
+		LessonsDataGrid.AddColumn("Вид", $"{nameof(Lesson.Type)}.{nameof(TypeOfLesson.Title)}");
+		LessonsDataGrid.AddColumn("Количество часов", nameof(Lesson.HoursAmount));
 	}
 }
