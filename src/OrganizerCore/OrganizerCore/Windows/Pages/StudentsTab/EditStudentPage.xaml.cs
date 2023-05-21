d09f015cfdc8c93c7a68f00fa1708456b0e462d4
Rapuška <mikalajsiatkouski@gmail.com>
2023-05-22 00:43:08 +0300

    EditStudentPage layout

---
+++ src/OrganizerCore/OrganizerCore/Windows/Pages/StudentsTab/EditStudentPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace OrganizerCore.Windows.Pages.StudentsTab;
+
+public partial class EditStudentPage : Page
+{
+	public EditStudentPage()
+	{
+		InitializeComponent();
+	}
+}
