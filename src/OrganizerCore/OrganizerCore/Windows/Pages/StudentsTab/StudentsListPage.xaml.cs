62c423fe39431710ae728fb24f9bbbe7bdc7b0a4
Rapuška <mikalajsiatkouski@gmail.com>
2023-05-21 20:31:34 +0300

    Students list page layout

---
+++ src/OrganizerCore/OrganizerCore/Windows/Pages/StudentsTab/StudentsListPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace OrganizerCore.Windows.Pages.StudentsTab;
+
+public partial class StudentsListPage : Page
+{
+	public StudentsListPage()
+	{
+		InitializeComponent();
+	}
+
+	private void IndividualCoursesButton_Click(object sender, RoutedEventArgs e)
+	{
+		throw new System.NotImplementedException();
+	}
+
+	private void GroupCoursesButton_Click(object sender, RoutedEventArgs e)
+	{
+		throw new System.NotImplementedException();
+	}
+}
