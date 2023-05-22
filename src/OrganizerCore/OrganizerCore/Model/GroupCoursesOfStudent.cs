c88d363b66ee4ca795e19c6ca54fb82739ccaeab
Rapuška <mikalajsiatkouski@gmail.com>
2023-05-23 02:11:06 +0300

    LessonsCount for group courses

--- src/OrganizerCore/OrganizerCore/Model/GroupCoursesOfStudent.cs
+++ src/OrganizerCore/OrganizerCore/Model/GroupCoursesOfStudent.cs
@@ -1,5 +1,6 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using OrganizerCore.DbWorking;
 
 namespace OrganizerCore.Model;
 
@@ -10,7 +11,11 @@ public class GroupCoursesOfStudent : Table
 	public       Group   Group     { get; set; } = null!;
 	public       string  Indicator { get; set; } = null!;
 
-	public int LessonsCount => throw new NotImplementedException();
+	public int LessonsCount => Group.Course.LessonsCount - CountOfHeldLessons;
+
+	private int CountOfHeldLessons
+		=> DataBaseConnection.Instance.CurrentContext.Schedules
+		                     .Count((s) => s.GroupCourse == this && s.IsHeld);
 
 	public override string ToString() => $"Курс {Group.Course} студента {Student} из группы {Group}";
 }
