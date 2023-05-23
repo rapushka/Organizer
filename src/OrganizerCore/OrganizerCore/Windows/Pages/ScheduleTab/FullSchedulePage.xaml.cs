09e93bd230b5d4da0c9376f93f4b07edb5a8b920
Rapuška <mikalajsiatkouski@gmail.com>
2023-05-23 18:00:41 +0300

    +FullSchedulePage

---
+++ src/OrganizerCore/OrganizerCore/Windows/Pages/ScheduleTab/FullSchedulePage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace OrganizerCore.Windows.Pages.ScheduleTab;
+
+public partial class FullSchedulePage : Page
+{
+	public FullSchedulePage()
+	{
+		InitializeComponent();
+	}
+}
