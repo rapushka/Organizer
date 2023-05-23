de1f21a0d9170320d2428b4cda1d81dbd953dd1e
Rapuška <mikalajsiatkouski@gmail.com>
2023-05-23 03:46:03 +0300

    groups page layout

---
+++ src/OrganizerCore/OrganizerCore/Windows/Pages/GroupsTab/GroupsListPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace OrganizerCore.Windows.Pages.GroupsTab;
+
+public partial class GroupsListPage : Page
+{
+	public GroupsListPage()
+	{
+		InitializeComponent();
+	}
+}
