6b18eabf6c6618d0e72168d96aa8cf973abd55a8
Rapuška <mikalajsiatkouski@gmail.com>
2023-05-23 05:13:31 +0300

    group save load

--- src/OrganizerCore/OrganizerCore/Windows/Pages/GroupsTab/EditGroupPage.xaml.cs
+++ src/OrganizerCore/OrganizerCore/Windows/Pages/GroupsTab/EditGroupPage.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using OrganizerCore.DbWorking;
 using OrganizerCore.Model;
+using OrganizerCore.Tools;
+using static System.Globalization.CultureInfo;
 
 namespace OrganizerCore.Windows.Pages.GroupsTab;
 
@@ -16,6 +19,43 @@ public partial class EditGroupPage
 	}
 
 	private static ApplicationContext Context => DataBaseConnection.Instance.CurrentContext;
+
+	private void Load()
+	{
+		TitleTextBox.Text = _group.Title;
+		CourseTextBox.Text = _group.Course.Title;
+		BeginDatePiker.Text = _group.BeginningDate.ToString(InvariantCulture);
+		EndDatePiker.Text = _group.EndingDate.ToString(InvariantCulture);
+		MinAgeTextBox.Text = _group.MinAge.ToString();
+		MaxAgeTextBox.Text = _group.MaxAge.ToString();
+		PlacesCountTextBox.Text = _group.MaxStudentsInGroupCount.ToString();
+	}
+
+	private bool TrySave()
+	{
+		var canParseBeginDate = DateTime.TryParse(BeginDatePiker.Text, out var beginDate);
+		var canParseEdnDate = DateTime.TryParse(EndDatePiker.Text, out var endDate);
+		var canParseMinAge = int.TryParse(MinAgeTextBox.Text, out var minAge);
+		var canParseMaxAge = int.TryParse(MaxAgeTextBox.Text, out var maxAge);
+		var canParsePlacesCount = int.TryParse(PlacesCountTextBox.Text, out var placesCount);
+
+		var valid = canParseBeginDate && canParseEdnDate && canParseMinAge && canParseMaxAge && canParsePlacesCount;
+
+		if (valid)
+		{
+			_group.Title = TitleTextBox.Text;
+			_group.Course.Title = CourseTextBox.Text;
+			_group.BeginningDate = beginDate;
+			_group.EndingDate = endDate;
+			_group.MinAge = minAge;
+			_group.MaxAge = maxAge;
+			_group.MaxStudentsInGroupCount = placesCount;
+			return true;
+		}
+
+		MessageBoxUtils.ShowError("Данные введены некорректно!");
+		return false;
+	}
 
 	private void AcceptButton_OnClick(object sender, RoutedEventArgs e)
 	{
