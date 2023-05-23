6cb01437523e8d40c0a66236cc2563681846160d
Rapuška <mikalajsiatkouski@gmail.com>
2023-05-23 18:14:28 +0300

    bindings

--- src/OrganizerCore/OrganizerCore/Windows/Pages/ScheduleTab/FullSchedulePage.xaml.cs
+++ src/OrganizerCore/OrganizerCore/Windows/Pages/ScheduleTab/FullSchedulePage.xaml.cs
@@ -1,11 +1,29 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
 
 namespace OrganizerCore.Windows.Pages.ScheduleTab;
 
-public partial class FullSchedulePage : Page
+public partial class FullSchedulePage
 {
 	public FullSchedulePage()
 	{
 		InitializeComponent();
+	}
+
+	private void FullSchedulePage_OnLoaded(object sender, RoutedEventArgs e) { }
+
+	private void UploadButton_OnClick(object sender, RoutedEventArgs e)
+	{
+		throw new System.NotImplementedException();
+	}
+
+	private void HeldButton_OnClick(object sender, RoutedEventArgs e)
+	{
+		throw new System.NotImplementedException();
+	}
+
+	private void ScheduleLessonButton_OnClick(object sender, RoutedEventArgs e)
+	{
+		throw new System.NotImplementedException();
 	}
 }
