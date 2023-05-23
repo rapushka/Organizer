ce5102b871fbd47ca4397a8ffb056d0c620483bc
Rapuška <mikalajsiatkouski@gmail.com>
2023-05-23 19:00:54 +0300

    +ScheduleView

---
+++ src/OrganizerCore/OrganizerCore/Model/Views/ScheduleView.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace OrganizerCore.Model.Views;
+
+// | Дата-Время | Курс | Занятие | Примечание | Проведено |
+public class ScheduleView
+{
+	private readonly bool _isGroup;
+	private readonly Group? _group;
+	private readonly IndividualCoursesOfStudent? _individualCourse;
+	private readonly Schedule _schedule;
+
+	public ScheduleView(IndividualCoursesOfStudent individualCourse, Schedule schedule)
+	{
+		_isGroup = false;
+		_individualCourse = individualCourse;
+		_schedule = schedule;
+	}
+
+	public ScheduleView(Group group, Schedule schedule)
+	{
+		_isGroup = true;
+		_group = group;
+		_schedule = schedule;
+	}
+
+	public DateTime ScheduledTime => _schedule.ScheduledTime;
+	public string   CourseTitle   => Course.Title;
+	public string   LessonType    => _isGroup ? _group!.Title : _individualCourse!.Student.FullName;
+	public string   Note          => _schedule.Note;
+	public bool     IsHeld        => _schedule.IsHeld;
+
+	private Course Course => _isGroup ? _group!.Course : _individualCourse!.Course;
+}
--- src/OrganizerCore/OrganizerCore/Windows/Pages/ScheduleTab/FullSchedulePage.xaml.cs
+++ src/OrganizerCore/OrganizerCore/Windows/Pages/ScheduleTab/FullSchedulePage.xaml.cs
@@ -1,29 +1,38 @@
 ﻿using System.Windows;
-using System.Windows.Controls;
 
 namespace OrganizerCore.Windows.Pages.ScheduleTab;
 
 public partial class FullSchedulePage
 {
-	public FullSchedulePage()
+	private bool _showHeld;
+	private bool _showNotHeld;
+
+	public FullSchedulePage() => InitializeComponent();
+
+	private void FullSchedulePage_OnLoaded(object sender, RoutedEventArgs e) => SetupTable();
+
+#region Table
+
+	private void SetupTable()
 	{
-		InitializeComponent();
+		
 	}
+	
+#endregion
 
-	private void FullSchedulePage_OnLoaded(object sender, RoutedEventArgs e) { }
+	private void UploadButton_OnClick(object sender, RoutedEventArgs e) { }
 
-	private void UploadButton_OnClick(object sender, RoutedEventArgs e)
+	private void HeldButton_OnClick(object sender, RoutedEventArgs e) { }
+
+	private void ScheduleLessonButton_OnClick(object sender, RoutedEventArgs e) { }
+
+	private void RadioButton_Checked(object sender, RoutedEventArgs e)
 	{
-		throw new System.NotImplementedException();
-	}
+		var all = ShowAllRadioButton.IsChecked!.Value;
+		var onlyHeld = OnlyHeldRadioButton.IsChecked!.Value;
+		var onlyNotHeld = OnlyNotHeldRadioButton.IsChecked!.Value;
 
-	private void HeldButton_OnClick(object sender, RoutedEventArgs e)
-	{
-		throw new System.NotImplementedException();
-	}
-
-	private void ScheduleLessonButton_OnClick(object sender, RoutedEventArgs e)
-	{
-		throw new System.NotImplementedException();
+		_showHeld = all || onlyHeld;
+		_showNotHeld = all || onlyNotHeld;
 	}
 }
