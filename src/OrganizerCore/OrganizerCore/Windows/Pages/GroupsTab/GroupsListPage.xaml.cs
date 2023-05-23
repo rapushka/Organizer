88526b671279476b9b32099a926fcdd6a9313127
Rapuška <mikalajsiatkouski@gmail.com>
2023-05-23 04:23:50 +0300

    Showing groups grid

--- src/OrganizerCore/OrganizerCore/Windows/Pages/GroupsTab/GroupsListPage.xaml.cs
+++ src/OrganizerCore/OrganizerCore/Windows/Pages/GroupsTab/GroupsListPage.xaml.cs
@@ -1,11 +1,71 @@
-﻿using System.Windows.Controls;
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Data;
+using OrganizerCore.DbWorking;
+using OrganizerCore.Model;
+using OrganizerCore.Tools.Extensions;
 
 namespace OrganizerCore.Windows.Pages.GroupsTab;
 
-public partial class GroupsListPage : Page
+public partial class GroupsListPage
 {
 	public GroupsListPage()
 	{
 		InitializeComponent();
 	}
+
+	private static IEnumerable<Course> Courses => DataBaseConnection.Instance.Observe<Course>();
+
+	private void GroupsListPage_OnLoaded(object sender, RoutedEventArgs e)
+	{
+		SetupStudentsDataGrid();
+		SetupStudentsColumns();
+	}
+
+#region Groups Table setup
+
+	private void SetupStudentsDataGrid()
+	{
+		var studentsViewSource = new CollectionViewSource
+		{
+			Source = DataBaseConnection.Instance.Observe<Group>(),
+		};
+
+		studentsViewSource.Filter += FilterGroups;
+		GroupsDataGrid.ItemsSource = studentsViewSource.View;
+	}
+
+	private void SetupStudentsColumns()
+	{
+		GroupsDataGrid.Columns.Clear();
+
+		GroupsDataGrid.AddTextColumn("ID", nameof(Group.Id), Visibility.Collapsed);
+		GroupsDataGrid.AddTextColumn("Название", nameof(Group.Title));
+		GroupsDataGrid.AddComboBoxColumn
+		(
+			header: "Курс",
+			binding: nameof(Group.Course),
+			itemsSource: Courses,
+			displayMemberPath: nameof(Course.Title),
+			selectedValuePath: nameof(Course.Id)
+		);
+		GroupsDataGrid.AddTextColumn("Дата начала", nameof(Group.BeginningDate));
+		GroupsDataGrid.AddTextColumn("Дата окончания", nameof(Group.EndingDate));
+		GroupsDataGrid.AddTextColumn("Минимальный возраст", nameof(Group.MinAge));
+		GroupsDataGrid.AddTextColumn("Максимальный возраст", nameof(Group.MaxAge));
+		GroupsDataGrid.AddTextColumn("Количество мест", nameof(Group.MaxStudentsInGroupCount));
+	}
+
+	private void FilterGroups(object sender, FilterEventArgs e)
+	{
+		var group = (Group)e.Item;
+
+		var fitsByTitle = group.Title.Contains(GroupTitleTextBox.Text);
+		var fitsByCourse = group.Course.Title.Contains(CourseTitleTextBox.Text);
+
+		e.Accepted = fitsByTitle && fitsByCourse;
+	}
+
+#endregion
 }
