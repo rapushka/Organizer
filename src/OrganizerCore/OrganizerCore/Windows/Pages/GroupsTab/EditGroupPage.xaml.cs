4dccd5cb5129b343d14f615fc58a04d5371c5111
Rapuška <mikalajsiatkouski@gmail.com>
2023-05-23 04:44:24 +0300

    Layout

---
+++ src/OrganizerCore/OrganizerCore/Windows/Pages/GroupsTab/EditGroupPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace OrganizerCore.Windows.Pages.GroupsTab;
+
+public partial class EditGroupPage : Page
+{
+	public EditGroupPage()
+	{
+		InitializeComponent();
+	}
+}
