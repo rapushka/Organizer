939b8510c441fa951a8cfe21cc021a97ad543abb
Rapuška <mikalajsiatkouski@gmail.com>
2023-05-24 12:40:11 +0300

    EditSchedulePage

---
+++ src/OrganizerCore/OrganizerCore/Windows/Pages/ScheduleTab/EditSchedulePage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace OrganizerCore.Windows.Pages.ScheduleTab;
+
+public partial class EditSchedulePage : Page
+{
+	public EditSchedulePage()
+	{
+		InitializeComponent();
+	}
+
+	private void CancelButton_Click(object sender, RoutedEventArgs e)
+	{
+		
+	}
+}
