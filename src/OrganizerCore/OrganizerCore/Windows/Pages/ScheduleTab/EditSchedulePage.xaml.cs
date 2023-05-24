6c960bc55bdfbe0988337d830f57f5ebccf27d85
Rapuška <mikalajsiatkouski@gmail.com>
2023-05-24 15:44:13 +0300

    bindings

--- src/OrganizerCore/OrganizerCore/Windows/Pages/ScheduleTab/EditSchedulePage.xaml.cs
+++ src/OrganizerCore/OrganizerCore/Windows/Pages/ScheduleTab/EditSchedulePage.xaml.cs
@@ -3,19 +3,18 @@ using System.Windows.Controls;
 
 namespace OrganizerCore.Windows.Pages.ScheduleTab;
 
-public partial class EditSchedulePage : Page
+public partial class EditSchedulePage
 {
 	public EditSchedulePage()
 	{
 		InitializeComponent();
 	}
 
-	private void CancelButton_Click(object sender, RoutedEventArgs e)
-	{
-	}
+	private void RadioButton_Click(object sender, RoutedEventArgs e) { }
 
-	private void RadioButton_Click(object sender, RoutedEventArgs e)
-	{
-		
-	}
+	private void ApplyButton_OnClick(object sender, RoutedEventArgs e) { }
+
+	private void CancelButton_OnClick(object sender, RoutedEventArgs e) { }
+
+	private void CourseComboBox_OnSelectionChanged(object sender, SelectionChangedEventArgs e) { }
 }
