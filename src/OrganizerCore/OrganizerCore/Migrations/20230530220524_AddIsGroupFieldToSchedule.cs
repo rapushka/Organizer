762127a782e040a5188373f67a18d5d72953dd8d
Rapuška <mikalajsiatkouski@gmail.com>
2023-05-31 01:08:51 +0300

    fix

---
+++ src/OrganizerCore/OrganizerCore/Migrations/20230530220524_AddIsGroupFieldToSchedule.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OrganizerCore.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIsGroupFieldToSchedule : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsGroup",
+                table: "Schedules",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsGroup",
+                table: "Schedules");
+        }
+    }
+}
